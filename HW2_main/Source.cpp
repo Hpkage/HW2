708b217755508a822e0d3fa23ca3ae3ac76e7aaf
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-19 21:43:39 -0800

    Add bool

--- HW2_main/Source.cpp
+++ HW2_main/Source.cpp
@@ -7,9 +7,9 @@
 #include <algorithm>
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::vector;
-using std::cin;
 using std::string;
 
 void InputNames(vector<string>) {
@@ -21,14 +21,24 @@ void InputNames(vector<string>) {
 	return;
 }
 
+bool DoesNameExist(const string, const vector<string>) {
+	cout << std::boolalpha << DoesNameExist << endl;
+	if (DoesNameExist) {
+		cout << "NameExists." << endl;
+	}
+	return 0;
+}
+
 int main(int argc, char** argv) {
+	int n;
 	void InputNames(vector<string>);
 	for (int i = 0; i < 10; i++) {
 		string name;
 		cout << "Please enter a name: " << endl;
 		getline(cin, name);
 		cout << endl;
-
 	}
+
 	return 0;
+	bool DoesNameExist(const string, const vector<string>);
 }
