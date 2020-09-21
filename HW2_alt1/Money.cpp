bfcdd483c01427ba9e4aad6c1da7440d7a1cf43f
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-21 00:14:43 -0800

    HW2_alt1

---
+++ HW2_alt1/Money.cpp
@@ -0,0 +1,18 @@
+//Money.cpp Hiromi Kageyama Sept. 20, 2020
+//This program will take the input of the user and output the sum of the inputs
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+
+	return 0;
+}
--- HW2_main/Source.cpp
+++ HW2_main/Source.cpp
@@ -12,6 +12,7 @@ using std::endl;
 using std::vector;
 using std::string;
 
+//sets the parameter for the main function
 void InputNames(vector<string>) {
 	vector<string> names;
 	string name;
@@ -21,24 +22,33 @@ void InputNames(vector<string>) {
 	return;
 }
 
+//searches for a name and returns true or false
 bool DoesNameExist(const string, const vector<string>) {
-	cout << std::boolalpha << DoesNameExist << endl;
-	if (DoesNameExist) {
-		cout << "NameExists." << endl;
+	vector<string> names;
+	bool nameExists = true;
+	if (names() > 10) {
+		nameExists = false;
 	}
 	return 0;
 }
 
+//main function of this program
 int main(int argc, char** argv) {
-	int n;
-	void InputNames(vector<string>);
-	for (int i = 0; i < 10; i++) {
+	int n, i;
+	for (i = 0; i < 10; i++) {
 		string name;
 		cout << "Please enter a name: " << endl;
 		getline(cin, name);
 		cout << endl;
+		void InputNames(vector<string>);
 	}
-
+	string nameExists;
+	cout << "Enter a name you entered to see if it exists: ";
+	cin >> nameExists;
+	if (DoesNameExist(nameExists) < 1);
+		cout << "It exists.";
+	else 		
+		cout << "It does not exist.";
+	cout << endl;
 	return 0;
-	bool DoesNameExist(const string, const vector<string>);
 }
